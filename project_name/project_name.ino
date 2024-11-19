b89de373cf4d805a25b2b5806cc533e18a2c8a12
BenUrbainINSA <burbain@insa-toulouse.fr>
2024-11-19 09:15:02 +0100

    Class menu en developpement

--- project_name/project_name.ino
+++ project_name/project_name.ino
@@ -5,11 +5,17 @@ Application myApplication;
 void setup() 
 {
   // put your setup code here, to run once:
-  myApplication.init();
+  //myApplication.init();
+  pinMode(16,OUTPUT);
+  pinMode(13,INPUT);
 }
 
 void loop() 
 {
   // put your main code here, to run repeatedly:
-  myApplication.run();
+  //myApplication.run();
+  digitalWrite(16,LOW);
+  delay(1000);
+  digitalWrite(16,HIGH);
+  
 }

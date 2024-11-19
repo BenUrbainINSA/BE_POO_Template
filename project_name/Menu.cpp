9540a59f027c0292b0b1f5e2f7ffacdd5093f3cd
BenUrbainINSA <burbain@insa-toulouse.fr>
2024-11-19 20:38:51 +0100

    j'ai commenté les fonction

--- project_name/Menu.cpp
+++ project_name/Menu.cpp
@@ -2,31 +2,35 @@
 
 Menu::Menu(int potPin, int button, rgb_lcd lcd) :  potentiometerPin(potPin), buttonPin(buttonPin), lcd(lcd), currentSelection(0), lastPotValue(-1){}
 
-void Menu::init(){
+//fonction qui initialise le menu, donc setup du boutton, du potentiometre et de l'écran.
+void Menu::init(){   
   pinMode(buttonPin,INPUT);
   pinMode(potentiometerPin,INPUT);
   lcd.begin(16,2);
   lcd.setRGB(colorR, colorG, colorB);
 
-  if (instruments.empty()) {
+  if (instruments.empty()) {    //Remplis la liste d'instrument. On peut ptt mettre une interuption ici 
     instruments.push_back("Clarinette");
     instruments.push_back("feur");
     instruments.push_back("Courgette");
   }
 }
 
+//Fonction qui met a jour l'affichage de l'ecran lorsque qu'il recoit une entrée. Potentiomètre fait defilier les instruments, boutton les selectionne.
 void Menu::update(){
   int potValue = analogRead(potentiometerPin);
-  Serial.println(potValue);
-  int mappedValue = map(potValue,0,1023,0,instruments.size() -1);
+  int mappedValue = map(potValue,0,1023,0,instruments.size() -1);   //on remap la valeur analogique sur la taille de la liste d'instruments 
 
-    if (mappedValue != lastPotValue) {
+    if (mappedValue != lastPotValue) {  //si la valeur lu est different on update l'ecran 
       currentSelection = mappedValue;
       displayMenu();
       lastPotValue = mappedValue;
-  }
+    }
+
+    //Si on appuis sur le boutton on selection l'instrument.
 }
 
+//Affiche le menu et l'intrument selectionné pour le moment 
 void Menu::displayMenu(){
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -36,6 +40,7 @@ void Menu::displayMenu(){
   lcd.print(instruments[currentSelection].c_str());
 }
 
+//Change l'affichage du menu lorsque l'on selection un objet pour montrer qu'on a interagis avec l'instrument 
 void Menu::handleSelection(){
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -47,6 +52,7 @@ void Menu::handleSelection(){
 }
 
 
+//Permet d'ajouter un instrument lors du debugage ou si on crée une fonction de creation d'instruments dynamique, à voir si on a le temps 
 void Menu::addIntrument(const std::string &instrument){
   Menu::instruments.push_back(instrument);
 }
